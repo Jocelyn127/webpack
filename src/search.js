e0be9074eb11f22e87eca7998078404a92af0124
Jocelyn127 <1270264022@qq.com>
2021-11-02 23:01:32 +0800

    use babel loader to parse JSX in react

--- src/search.js
+++ src/search.js
@@ -1,1 +1,15 @@
-document.write('search page');
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends  React.Component {
+    render(){
+        return <div>Search text</div>
+    }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+)
