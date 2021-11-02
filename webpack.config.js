d8ba2749963ca55b3ffdc4343c91f0432035b20d
Jocelyn127 <1270264022@qq.com>
2021-11-03 00:12:52 +0800

    url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,14 @@ module.exports = {
         },
         {
             test: /.(png|jpg|gif|jpeg)$/,
-            use: 'file-loader'
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10240
+                    }
+                }
+            ]
         },
         {
             test: /.(woff|woff2|eot|ttf|otf)$/,
